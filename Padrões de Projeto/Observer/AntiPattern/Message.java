e0da7b29b9547ac935554d0f277714353f47f868
Camila Pacheco <camilapacheco.contato@gmail.com>
2022-11-30 21:43:15 -0300

    observer

--- Padrões de Projeto/Observer/AntiPattern/Message.java
+++
@@ -1,9 +0,0 @@
-public class Message {
-
-	public static void mandarNotificacao(String nome, String placa, String tempo_restante) {
-		System.out.println(nome + placa + tempo_restante);
-		
-	}
-
-}
-
